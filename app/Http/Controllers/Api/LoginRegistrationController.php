da0b4f46e36eca177912c7c155f05f21541188b4
AkhileshAdhikari <akhileshadhikaria1@gmail.com>
2023-11-23 23:14:58 +0545

    test commit

---
+++ app/Http/Controllers/Api/LoginRegistrationController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class LoginRegistrationController extends Controller
+{
+    //Registation API (POST, formdata)
+    public function register(Request $request){
+
+        // Validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
+
+        User::create([
+            "name" => $request->name,
+            "email" =>$request->email,
+            "password" => Hash::make($request->password),
+        ]);
+
+        return response()->json([
+            "status" => true,
+            "message" => "New User Created Successfully",
+        ]);
+    }
+
+    // Login API (POST, formdata)
+    public function login(Request $request) {
+        // Validation
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        // JWT auth and attempt
+        $token = JWTAuth::attempt([
+            "email" => $request->email,
+            "password" => $request->password,
+        ]);
+
+        // Response
+        if (!empty($token)){
+            return response()->json([
+                "status" => true,
+                "message" => "User logged in successfully.",
+                "token" => $token
+            ]);
+        }
+        return response()->json([
+            "status" => false,
+            "message" => "Invalid ligin details."
+        ]);
+    }
+
+    // Profile API (GET)
+    public function profile() {
+
+        $userdata = auth()->user();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Profile data",
+            "user" => $userdata,
+        ]);
+
+    }
+
+    // Refresh token API (GET) 
+    public function refreshToken() {
+
+        $newToken = auth()->refresh();
+        // $newToken = "123";
+
+        return response()->json([
+            "status" => true,
+            "message" => "New token generated.",
+            "token" => $newToken,
+        ]);
+        
+    }
+
+    // Logout API (GET)
+    public function logout() {
+
+        auth()->logout();
+
+        return response()->json([
+            "status" => true,
+            "message" => "User loggod out successfully",
+        ]);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginRegistrationController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post("register", [LoginRegistrationController::class, "register"]);
+Route::post("login", [LoginRegistrationController::class, "login"]);
+
+Route::group([
+    "middleware" => ["auth:api"]
+], function() {
+    Route::get("profile", [LoginRegistrationController::class, "profile"]);
+    Route::get("refresh", [LoginRegistrationController::class, "refreshToken"]);
+    Route::get("logout", [LoginRegistrationController::class, "logout"]);
+});
